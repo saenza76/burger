00f42c2b48beee07cce5f72efa606783921004c3
ANDRES SAENZ <SAENZA76@YAHOO.COM>
2016-11-05 00:14:04 -0500

    working app, basic design

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: process.env.db_host || "localhost",
+	user: process.env.db_user || "root",
+	password: process.env.db_pw || "1Happier1",
+	database: process.env.db || "burgers_db"
+});
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
